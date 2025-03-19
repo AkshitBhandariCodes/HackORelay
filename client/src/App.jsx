2007007de1df6e6fdeb8579648396457c0666699
Sohan Rout <sohanrout84@gmail.com>
2025-03-19 18:27:15 +0530

    added components to the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Button, buttonVariants } from './components/ui/button';
+'use client'
+import React from "react";
+import { Button, buttonVariants } from "./components/ui/button";
+import Navbar from "./components/ui/Navbar";
+import Hero from "./components/ui/Hero";
+import Sponsor from "./components/ui/Sponser";
+import About from "./components/ui/About";
+import Theme from "./components/ui/Theme";
+import Prize from "./components/ui/Prize";
+import Gallery from "./components/ui/Gallery";
+import FAQ from "./components/ui/Faq";
+import Footer from "./components/ui/Footer";
+
 const App = () => {
-return (
-    <div className='m-10 p-10'>
-<div className='text-2xl text-black'>Testing Tailwind</div>
-<Button variant="destructive" className='text-white mt-4'>Testing ShadCN</Button>
-</div>
-)
-}
-export default App
+  return (
+    <div className="m-10 p-10">
+        <Navbar/>
+        <Hero/>
+        <Sponsor/>
+        <About/>
+        <Theme/>
+        <Prize/>
+        <Gallery/>
+        <FAQ/>
+        <Footer/>
+    </div>
+  );
+};
+
+export default App;
