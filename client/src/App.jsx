2e81474a74c58a8c4384fae94c5b12731feb3d8c
Sohan Rout <sohanrout84@gmail.com>
2025-03-19 19:06:05 +0530

    fixed file components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 'use client'
 import React from "react";
 import { Button, buttonVariants } from "./components/ui/button";
-import Navbar from "./components/ui/Navbar";
-import Hero from "./components/ui/Hero";
-import Sponsor from "./components/ui/Sponser";
-import About from "./components/ui/About";
-import Theme from "./components/ui/Theme";
-import Prize from "./components/ui/Prize";
-import Gallery from "./components/ui/Gallery";
-import FAQ from "./components/ui/Faq";
-import Footer from "./components/ui/Footer";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Sponsor from "@/components/sponser";
+import About from "@/components/Sponser";
+import Theme from "@/components/Theme";
+import Prize from "@/components/Prize";
+import Gallery from "@/components/Gallery";
+import FAQ from "@/components/Faq";
+import Footer from "@/components/Footer";
 
 const App = () => {
   return (
-    <div className="m-10 p-10">
+    <div>
         <Navbar/>
         <Hero/>
         <Sponsor/>
